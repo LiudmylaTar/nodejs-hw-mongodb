a9d9704a6b284480890431e5db28b348803400f4
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-07-16 14:07:17 +0300

    HW-4 first part - validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,15 +8,30 @@ import {
   updateContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 
 const router = Router();
 router.get('/', ctrlWrapper(getContactsController));
 
-router.get('/:contactId', ctrlWrapper(getContactController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactController));
 
-router.post('/', ctrlWrapper(createContactController));
+router.post(
+  '/',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.delete('/:id', ctrlWrapper(deleteContactController));
-router.patch('/:id', ctrlWrapper(updateContactController));
+router.delete('/:id', isValidId, ctrlWrapper(deleteContactController));
+router.patch(
+  '/:id',
+  validateBody(updateContactSchema),
+  isValidId,
+  ctrlWrapper(updateContactController),
+);
 
 export default router;
